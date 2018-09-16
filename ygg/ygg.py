3f76d23a7c4edfa6b58ecd721c38743800799ea8
Florent David <florent.david@gmail.com>
2018-09-16 17:51:56 +0200

    Fix #12

--- ygg/ygg.py
+++ ygg/ygg.py
@@ -21,7 +21,7 @@ class YGG(TorrentProvider, MovieProvider):
     """
 
     url_scheme = 'https'
-    url_hostname = 'www.yggtorrent.is'
+    url_hostname = 'yggtorrent.to'
     limit = 50
     http_time_between_calls = 0
 
