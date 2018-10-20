da16af4ce8e4d77c30208e903e3d18e266bc071d
Florent David <florent.david@gmail.com>
2018-10-20 16:31:39 +0200

    Update hostname

--- ygg/ygg.py
+++ ygg/ygg.py
@@ -21,7 +21,7 @@ class YGG(TorrentProvider, MovieProvider):
     """
 
     url_scheme = 'https'
-    url_hostname = 'www3.yggtorrent.to'
+    url_hostname = 'www6.yggtorrent.to'
     limit = 50
     http_time_between_calls = 0
 
